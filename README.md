78d52fa3d92f500ad003a6efa874b159e16649df
Oliver Heywood <oheywood@four51.com>
2021-01-25 10:29:46 -0600

    Initial commit. Bringing in result of apollo tutorial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # MACHathon-GraphCatalog
 Combine commerce and content by querying your product catalog with this GraphQL gateway API.
+
+## Get Started 
+1. Clone this repo `git clone https://github.com/ordercloud-api/MACHathon-GraphCatalog.git`
+2. Install dependencies in `./server` with `npm install`
+3. `npm start`
+4. Naviate to [http://localhost:4000/](http://localhost:4000/) where you shoud see Apollo GraphQL Playground ([Docs](https://www.apollographql.com/docs/apollo-server/testing/graphql-playground/))
+5. Try out some API requests!
+
+## Tools we're using 
+- [Node JS](https://nodejs.org/en/about/)
+- [Apollo Server](https://www.apollographql.com/docs/apollo-server/) (GraphQL Implementation)
---
+++ server/src/resolvers.js
@@ -0,0 +1,76 @@
+module.exports = {
+    Query: {
+      launches: (_, __, { dataSources }) =>
+        dataSources.launchAPI.getAllLaunches(),
+      launch: (_, { id }, { dataSources }) =>
+        dataSources.launchAPI.getLaunchById({ launchId: id }),
+      me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+    },
+    Mutation: {
+      login: async (_, { email }, { dataSources }) => {
+        const user = await dataSources.userAPI.findOrCreateUser({ email });
+        if (user) {
+          user.token = Buffer.from(email).toString('base64');
+          return user;
+        }
+      },
+      bookTrips: async (_, { launchIds }, { dataSources }) => {
+        const results = await dataSources.userAPI.bookTrips({ launchIds });
+        const launches = await dataSources.launchAPI.getLaunchesByIds({
+          launchIds,
+        });
+    
+        return {
+          success: results && results.length === launchIds.length,
+          message:
+            results.length === launchIds.length
+              ? 'trips booked successfully'
+              : `the following launches couldn't be booked: ${launchIds.filter(
+                  id => !results.includes(id),
+                )}`,
+          launches,
+        };
+      },
+      cancelTrip: async (_, { launchId }, { dataSources }) => {
+        const result = await dataSources.userAPI.cancelTrip({ launchId });
+    
+        if (!result)
+          return {
+            success: false,
+            message: 'failed to cancel trip',
+          };
+    
+        const launch = await dataSources.launchAPI.getLaunchById({ launchId });
+        return {
+          success: true,
+          message: 'trip cancelled',
+          launches: [launch],
+        };
+      },
+    },
+    Mission: {
+        // The default size is 'LARGE' if not provided
+        missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+          return size === 'SMALL'
+            ? mission.missionPatchSmall
+            : mission.missionPatchLarge;
+        }
+    },
+    Launch: {
+      isBooked: async (launch, _, { dataSources }) =>
+        dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id }),
+    },
+    User: {
+      trips: async (_, __, { dataSources }) => {
+        // get ids of launches by user
+        const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+        if (!launchIds.length) return [];
+        // look up those launches by their ids
+        return (
+          dataSources.launchAPI.getLaunchesByIds({
+            launchIds,
+          }) || []
+        );
+      },
+    },
+  };
